ed4346992ce79f21a37dbf5a06a2b3b0fd027df9
pepe <pepe@pepePc.lan>
2017-01-04 13:35:32 -0600

    Initial commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+  login: any;
+  constructor(public navCtrl: NavController) {
+    this.login = "Login";
+  }
+
+}
